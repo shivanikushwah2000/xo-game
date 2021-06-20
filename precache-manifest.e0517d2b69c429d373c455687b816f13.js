c47f0502d51f28e1f586bf3c9756e1039324e7a3
shivanikushwah2000 <shivani.kushwah@mirandahouse.ac.in>
2021-06-20 20:42:22 +0530

    Updates

---
+++ precache-manifest.e0517d2b69c429d373c455687b816f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cb53598e52aea6596d020804f263b6",
+    "url": "/xo-game/index.html"
+  },
+  {
+    "revision": "c4e93ecb2c392b4ac764",
+    "url": "/xo-game/static/css/main.a6b8b413.chunk.css"
+  },
+  {
+    "revision": "13c0425173494107c0fb",
+    "url": "/xo-game/static/js/2.a5ca6e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xo-game/static/js/2.a5ca6e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e93ecb2c392b4ac764",
+    "url": "/xo-game/static/js/main.2c574946.chunk.js"
+  },
+  {
+    "revision": "f38048bf7f48f16da434",
+    "url": "/xo-game/static/js/runtime-main.26594c43.js"
+  }
+]);
